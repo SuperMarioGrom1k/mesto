019ced7f03477db17d0cad2999bcd3096a37a236
SuperMarioGrom1k <98209749+SuperMarioGrom1k@users.noreply.github.com>
2022-04-03 15:34:22 +0300

    fixNoPopop

--- script/index.js
+++ script/index.js
@@ -2,25 +2,16 @@
 const editProfile = document.querySelector(".profile__edit-button");
 const modalWindow = document.querySelector(".popup");
 const modalCloseBtn = modalWindow.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_name');
+let subTextInput = formElement.querySelector('.popup__input_subtext');
+let nameProfile = document.querySelector('.profile__name');
+let subTextProfile = document.querySelector('.profile__about');
 
 
 function toggleModalWindow() {
     modalWindow.classList.toggle('popup__open');
 }
-
-editProfile.addEventListener('click', toggleModalWindow);
-modalCloseBtn.addEventListener('click', toggleModalWindow);
-
-
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_name');
-let subTextInput = formElement.querySelector('.popup__input_subtext');
-
-
-let nameProfile = document.querySelector('.profile__name');
-let subTextProfile = document.querySelector('.profile__about');
-
 
 
 function formSubmitHandler(evt) {
@@ -38,6 +29,9 @@ function formSubmitHandler(evt) {
 
 
 formElement.addEventListener('submit', formSubmitHandler);
+editProfile.addEventListener('click', toggleModalWindow);
+modalCloseBtn.addEventListener('click', toggleModalWindow);
+
 
 
 
