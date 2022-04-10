6f3c443080ee442fb393fadccfb153fe985a5956
SuperMarioGrom1k <98209749+SuperMarioGrom1k@users.noreply.github.com>
2022-04-10 18:42:04 +0300

    fixNoPopop

--- script/index.js
+++ script/index.js
@@ -2,39 +2,25 @@ const editProfile = document.querySelector(".profile__edit-button");
 const modalWindow = document.querySelector(".popup");
 const modalCloseBtn = modalWindow.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_name');
-let subTextInput = formElement.querySelector('.popup__input_subtext');
+let nameInput = formElement.querySelector('.popup__input-name');
+let subTextInput = formElement.querySelector('.popup__input-subtext');
 let nameProfile = document.querySelector('.profile__name');
 let subTextProfile = document.querySelector('.profile__about');
 
+const openPopupHandler = () => modalWindow.classList.contains('popup') && modalWindow.classList.add('popup_open');
+const closePopupHandler = () => modalWindow.classList.contains('popup') && modalWindow.classList.remove('popup_open');
 
-function toggleModalWindow() {
-    modalWindow.classList.toggle('popup_open');
+nameInput.value = nameProfile.textContent;
+subTextInput.value = subTextProfile.textContent;
 
-}
-
-function toggleModalCloseBtn() {
-    modalCloseBtn.classList.toggle('popup__close')
-
-
-}
-
-    nameInput.value = nameProfile.textContent;
-    subTextInput.value = subTextProfile.textContent;
-
-
-
-
-    function formSubmitHandler(evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     subTextProfile.textContent = subTextInput.value;
-    toggleModalWindow()
-
+    closePopupHandler();
 }
 
 
-modalCloseBtn.addEventListener('click', toggleModalCloseBtn);
-editProfile.addEventListener('click', toggleModalWindow);
-modalCloseBtn.addEventListener('click', toggleModalWindow);
+editProfile.addEventListener('click', openPopupHandler);
+modalCloseBtn.addEventListener('click', closePopupHandler);
 formElement.addEventListener('submit', formSubmitHandler);
