e04bb1812d9503e445bced8793ebf96827a2b031
SuperMarioGrom1k <98209749+SuperMarioGrom1k@users.noreply.github.com>
2022-04-27 19:03:44 +0300

    44

--- script/index.js
+++ script/index.js
@@ -2,16 +2,22 @@ const editProfile = document.querySelector(".profile__edit-button");
 const modalWindow = document.querySelector(".popup");
 const modalCloseBtn = modalWindow.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input-name');
-let subTextInput = formElement.querySelector('.popup__input-subtext');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let subTextInput = formElement.querySelector('.popup__input_type_about');
 let nameProfile = document.querySelector('.profile__name');
 let subTextProfile = document.querySelector('.profile__about');
-const openPopupHandler = () => modalWindow.classList.contains('popup')  && modalWindow.classList.add('popup_open');
-const closePopupHandler = () => modalWindow.classList.contains('popup') && (nameInput.value = nameProfile.textContent)
-    && (subTextInput.value = subTextProfile.textContent)  &&  modalWindow.classList.remove('popup_open');
 
-nameInput.value = nameProfile.textContent;
-subTextInput.value = subTextProfile.textContent;
+
+function openPopupHandler() {
+    modalWindow.classList.add('popup_open');
+    nameInput.value = nameProfile.textContent;
+    subTextInput.value = subTextProfile.textContent;
+}
+
+
+function closePopupHandler() {
+    modalWindow.classList.remove('popup_open');
+}
 
 
 function formSubmitHandler(evt) {
